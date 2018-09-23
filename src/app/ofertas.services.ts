917901bbaf3204e2cebc2b4e28a222cabf6f69fd
douglasgmsantos <douglas_motasts@outlook.com>
2018-09-23 18:46:03 -0300

    Corrigindo projeto do passaro urbano

---
+++ src/app/ofertas.services.ts
@@ -0,0 +1,48 @@
+import {Oferta} from './shared/oferta.model'
+import {Http} from '@angular/http'
+import {Injectable} from '@angular/core'
+
+import {URL_API} from './app.api'
+
+
+@Injectable()
+export class OfertasService{
+
+    constructor(private http:Http){}
+
+    public getOfertas():Promise<Oferta[]>{
+        return this.http.get(`${URL_API}/ofertas?destaque=true`)
+            .toPromise()
+            .then( (resposta: any) => resposta.json() )
+    }
+
+
+    public getOfertasPorCategoria(categoria: string):Promise<Oferta[]> {
+        return this.http.get(`${URL_API}/ofertas?categoria=${categoria}`)
+        .toPromise()
+        .then( (resposta: any) => resposta.json() )
+    }
+
+    public getOfertasPorId(id: number):Promise<Oferta> {
+        return this.http.get(`${URL_API}/ofertas?id=${id}`)
+        .toPromise()
+        .then( (resposta: any) => resposta.json()[0] )
+    }
+
+    public getComoUsarOfertaPorId(id: number):Promise<string>{
+        return this.http.get(`${URL_API}/como-usar?id=${id}`)
+                .toPromise()
+                .then((resposta : any) => {
+                    return resposta.json()[0].descricao
+                });
+    }
+
+    public getOndeFicaOfertaPorId(id: number):Promise<string>{
+        return this.http.get(`${URL_API}/onde-fica?id=${id}`)
+                .toPromise()
+                .then((resposta : any) => {
+                    console.log(resposta.json())
+                    return resposta.json()[0].descricao
+                });
+    }
+}
